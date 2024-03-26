78102a2f8802b49eade78a5ce065543f3064b421
youaresogay22 <youaresogayee@gmail.com>
2024-03-26 14:02:31 +0900

    [chore] 유레카 서버 적용

---
+++ src/main/java/shop/bookbom/eureka/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package shop.bookbom.eureka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity(debug = false)
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+
+    }
+}
