40babffbcbab4bc13b86309795e804152c57a0bc
youaresogay22 <youaresogayee@gmail.com>
2024-03-28 15:44:17 +0900

    [Chore] main 수정

--- src/main/java/shop/bookbom/eureka/EurekaApplication.java
+++ src/main/java/shop/bookbom/eureka/EurekaApplication.java
@@ -3,9 +3,6 @@ package shop.bookbom.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -14,11 +11,4 @@ public class EurekaApplication {
         SpringApplication.run(EurekaApplication.class, args);
     }
 
-    @EnableWebSecurity
-    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-        }
-    }
 }
